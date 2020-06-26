ef10a9927fcec618374a34a514970a89c310d676
xiongzhangzhe <xiongzhangzhe@bytedance.com>
2020-06-26 23:08:51 +0800

    322

---
+++ Algorithm/Algorithm/322_零钱兑换.cpp
@@ -0,0 +1,45 @@
+//
+//  322_零钱兑换.cpp
+//  Algorithm
+//
+//  Created by zack on 2020/6/26.
+//  Copyright © 2020 zack. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     *思路：递推思想，从总额=1开始计算所需硬币个数，逐个推导出总额=amount时所需硬币个数
+     */
+    int coinChange(vector<int>& coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        if (coins.size() == 0) {
+            return -1;
+        }
+        
+        vector<int> dp = vector<int>(amount+1);  //每个数组元素表示凑成总金额dp[i]所需的最少的硬币个数
+        for (int i = 1; i <= amount; i++) {
+            int min = INT_MAX;
+            for (int j = 0; j < coins.size(); j++) {
+                if (i < coins[j] || dp[i - coins[j]] < 0 || dp[i - coins[j]] >= min) continue;
+                min = dp[i - coins[j]];
+            }
+            
+            if (min == INT_MAX) {
+                dp[i] = -1;
+            } else {
+                dp[i] = min + 1;
+            }
+        }
+        
+        return dp[amount];
+    }
+};
